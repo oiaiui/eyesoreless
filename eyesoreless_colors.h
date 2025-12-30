456abed77bb77ba19c9a571a62c17a19185933cd
oiaiui <dbnull.git@gmail.com>
2025-12-30 01:47:58 +0000

    Adds header

---
+++ eyesoreless_colors.h
@@ -0,0 +1,26 @@
+// Header file that defines a few colours for my suckless software usage
+
+#ifndef EYESORELESS_COLORS_H
+#define EYESORELESS_COLORS_H
+
+// Normal
+static const char esrls_n_black[] 	= "#161616";
+static const char esrls_n_red[] 	= "#7f323a";
+static const char esrls_n_green[] 	= "#305e26";
+static const char esrls_n_yellow[] 	= "#664d00";
+static const char esrls_n_blue[] 	= "#1e5486";
+static const char esrls_n_magenta[] 	= "#5a4380";
+static const char esrls_n_cyan[] 	= "#00615d";
+static const char esrls_n_white[] 	= "#bebebe";
+
+// Brighter (or stronger etc.)
+static const char esrls_b_black[] 	= "#2e2e2e";
+static const char esrls_b_red[] 	= "#f97f81";
+static const char esrls_b_green[] 	= "#73c061";
+static const char esrls_b_yellow[] 	= "#cea401";
+static const char esrls_b_blue[] 	= "#57aeff";
+static const char esrls_b_magenta[] 	= "#b891f9";
+static const char esrls_b_cyan[] 	= "#00c5be";
+static const char esrls_b_white[] 	= "#dedede";
+
+#endif
